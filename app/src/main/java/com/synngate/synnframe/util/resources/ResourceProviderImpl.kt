1f40ec0f530f36f1589fe098f9b716f5908adbf8
slax <slax.xals@gmail.com>
2025-06-17 15:28:28 +0300

    fix resource provider

--- app/src/main/java/com/synngate/synnframe/util/resources/ResourceProviderImpl.kt
+++ app/src/main/java/com/synngate/synnframe/util/resources/ResourceProviderImpl.kt
@@ -2,16 +2,26 @@ package com.synngate.synnframe.util.resources
 
 import android.content.Context
 import androidx.annotation.StringRes
+import com.synngate.synnframe.presentation.util.LocaleHelper
+import java.util.Locale
 
 class ResourceProviderImpl(
     private val context: Context
 ) : ResourceProvider {
 
     override fun getString(@StringRes resId: Int): String {
-        return context.getString(resId)
+        // Получаем текущую локаль из системы
+        val currentLocale = Locale.getDefault()
+        // Создаем контекст с актуальной локалью
+        val localizedContext = LocaleHelper.updateLocale(context, currentLocale.language)
+        return localizedContext.getString(resId)
     }
 
     override fun getString(@StringRes resId: Int, vararg formatArgs: Any): String {
-        return context.getString(resId, *formatArgs)
+        // Получаем текущую локаль из системы
+        val currentLocale = Locale.getDefault()
+        // Создаем контекст с актуальной локалью
+        val localizedContext = LocaleHelper.updateLocale(context, currentLocale.language)
+        return localizedContext.getString(resId, *formatArgs)
     }
 }
