e93c2d74108755e1f82372f4a7452cef08b40add
slax <slax.xals@gmail.com>
2025-07-08 10:21:38 +0300

    v 1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.synngate.synnframe"
         minSdk = 28
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.3"
+        versionCode = 5
+        versionName = "1.0.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
